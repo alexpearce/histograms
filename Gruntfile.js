a23c670488915cf5b150a714b40a38fb31165a8a
Alex Pearce <alex@alexpearce.me>
2014-04-22 13:24:42 +0100

    Upgrade repository to an npm module, using Grunt as a task runner.

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // Concatenate all our source JavaScript files in to one, placed in dist/
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: [
+          'src/d3.chart.BaseChart.js',
+          'src/d3.chart.AxesChart.js',
+          'src/d3.chart.Histogram.js',
+          'src/d3.chart.HistogramZoom.js'
+        ],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    // Minify the result of the concatenation, creating an additional file
+    uglify: {
+      options: {
+        banner: '/* <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    // Check all JavaScript files for proper syntax
+    jshint: {
+      files: ['Gruntfile.js', 'src/*.js'],
+      options: {
+        // Tell JSHint about global vars; console is read-only, module is not
+        globals: {
+          console: false,
+          module: true
+        }
+      }
+    },
+    // Run JSHint on files changes
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'concat', 'uglify']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('watch', ['jshint', 'concat', 'uglify']);
+};
--- README.md
+++ README.md
@@ -3,6 +3,29 @@ Histograms
 
 Plot [histograms](https://en.wikipedia.org/wiki/Histogram) with [D3](http://d3js.org/).
 
+Contributing
+------------
+
+All the JavaScript files are under `src/`.
+To build the production scripts `d3.chart.histograms.js` and `d3.chart.histograms.min.js` you'll need [Grunt](http://gruntjs.com/).
+
+Clone the repository
+
+    git clone https://github.com/alexpearce/histograms.git
+
+and then install the development dependencies
+
+    npm install .
+
+Finally, run
+
+    grunt
+
+The output is placed in `dist/`.
+
+To have Grunt monitor files in `src/` during development, run `grunt watch`.
+Grunt will automatically update the files in `dist/`, and alert you of any syntax errors with [JSHint](http://www.jshint.com/).
+
 License
 -------
 

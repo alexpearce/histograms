000431e5459743c86dd9868523bbe34c73a72c5b
Alex Pearce <alex@alexpearce.me>
2014-06-02 15:31:07 +0100

    Update examples to show overlay histograms.

--- examples/javascripts/examples.js
+++ examples/javascripts/examples.js
@@ -31,19 +31,34 @@
     }
     return rtn;
   };
-  var gaussian = formatData(gaussianData),
-      landau = formatData(landauData);
-  var hGauss = makeHistogram(d3.select('#h1')),
+
+  // Define our datasets
+  var gaussian = {
+      colour: 'red',
+      values: formatData(gaussianData)
+  };
+  var landau = {
+    colour: 'green',
+    values: formatData(landauData)
+  };
+  var steps = {
+    colour: 'blue',
+    values: formatData({
+        'values': [0, 10, 20, 30, 40, 50],
+        'binning': [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6]],
+        'uncertainties': [0]
+    })
+  };
+
+  // Draw charts
+  var hMulti = makeHistogram(d3.select('#h1')),
       hLandau = makeHistogram(d3.select('#h2')),
       hTest = makeHistogram(d3.select('#h3'));
   var h2DGauss = make2DHistogram(d3.select('#h4'));
 
-  hGauss.draw(gaussian);
-  hLandau.draw(landau);
-  hTest.draw(formatData({
-    'values': [0, 1, 2, 3, 4, 5],
-    'binning': [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6]],
-    'uncertainties': [0]
-  }));
+  // Add data
+  hMulti.draw([gaussian, landau, steps]);
+  hLandau.draw([landau]);
+  hTest.draw([steps]);
   h2DGauss.draw(data2d);
 })(window, window.document);

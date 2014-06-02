185c6a7e008ba7778e9e2ab175cded664fce2218
Alex Pearce <alex@alexpearce.me>
2014-06-02 10:17:04 +0100

    Add compiled files for v0.0.2.

--- dist/d3.chart.histograms.js
+++ dist/d3.chart.histograms.js
@@ -12,6 +12,7 @@
   d3.chart('BaseChart', {
     initialize: function() {
       var chart = this;
+      chart.base.classed('BaseChart', true);
 
       chart.margins = {
         top: 10,
@@ -80,7 +81,7 @@
   d3.chart('BaseChart').extend('AxesChart', {
     initialize: function() {
       var chart = this;
-      chart.base.classed('AxesChart');
+      chart.base.classed('AxesChart', true);
 
       // Define blank x- and y-axis labels
       chart._xAxisLabel = '';
@@ -288,13 +289,13 @@
   d3.chart('AxesChart').extend('Histogram', {
     initialize: function() {
       var chart = this;
-      chart.base.classed('Histogram');
+      chart.base.classed('Histogram', true);
 
       // Get inner 'canvas'
       var innerG = chart.base.select('g');
 
-      // Draw the line
-      chart.layers.line = innerG.append('g')
+      // Draw the line underneath the axes, but above the grid
+      chart.layers.line = innerG.insert('g', '.axis')
         .classed('line', true);
       // Then the uncertainties
       // chart.layers.errors = innerG.append('g')
@@ -334,7 +335,34 @@
         }
       });
 
-	/*
+      /*
+      chart.layer('points', chart.layers.points, {
+        // Prepare data for binding, returning data join
+        dataBind: function(data) {
+          chart.data = data;
+
+          return this.selectAll('circle').data(data);
+        },
+        // Append the expected elements and set their attributes
+        insert: function() {
+          return this.append('circle').classed('point', true);
+        },
+        // Define lifecycle events
+        events: {
+          // Update bar attributes to reflect the data
+          enter: function() {
+            return this
+              .attr('r', 2)
+              .attr('cx', function(d) {
+                return (chart.xScale(d.x) + chart.xScale(d.dx))/2.;
+              })
+              .attr('cy', function(d) {
+                return chart.yScale(d.y);
+              });
+          }
+        }
+      });
+
       chart.layer('errors', chart.layers.errors, {
         // Prepare data for binding, returning data join
         dataBind: function(data) {
@@ -392,104 +420,121 @@
   });
 })(window.d3);
 ;(function(d3, undefined) {
-    'use strict';
-    d3.chart('AxesChart').extend('2DHistogram', {
-	initialize: function() {
-	    var chart = this;
-	    chart.base.classed('2DHistogram');
-	    
+  'use strict';
+  d3.chart('AxesChart').extend('Histogram2D', {
+    initialize: function() {
+      var chart = this;
+      chart.base.classed('Histogram2D', true);
 
-	    //Transfom scale for the z-axis
-	    chart.zScale = d3.scale.linear()
-		.range(["#2c7bb6", "#ffffbf", "#d7191c"])
-		.interpolate(d3.interpolateHcl);
-	    // Get inner 'canvas'
-	    var innerG = chart.base.select('g');
+      //Transfom scale for the z-axis
+      chart.zScale = d3.scale.linear()
+        .range(['#2c7bb6', '#ffffbf', '#d7191c'])
+        .interpolate(d3.interpolateHcl);
 
-	    //gain space on the right in order to insert the zscale 
-	    chart.margins.right = 100;
+      // Get inner 'canvas'
+      var innerG = chart.base.select('g');
 
-	    //legend for Z axis
-	    chart.areas.legend = chart.base.append('g')
-		.classed("legend",true);
+      //gain space on the right in order to insert the zscale
+      chart.margins.right = 100;
 
-	    // add the tiles to layers
-	    chart.layers.tiles = innerG.append('g')
-		.classed('tiles', true);
+      //legend for Z axis
+      chart.areas.legend = chart.base.append('g')
+        .classed('legend', true)
+        .attr('transform', function(d, i) { return 'translate(' + (chart.width() + chart.margins.left + 10) + ',' + chart.margins.top + ')'; });
 
-	    chart.layer('rect', chart.layers.tiles, {
-		dataBind: function(data) {
-		    return this.selectAll('rect').data(data.data);
-		},
-		insert: function() {
-		    return this.append('rect')
-			.classed('tiles', true);
-		},
-		events: {
-		    enter: function() {
-			return this
-			    .attr("x",function(d) { return chart.xScale(d.xlow); })
-			    .attr("y",function(d) { return chart.yScale(d.yup); })
-			    .attr("width",function(d) { return chart.xScale(d.xup-d.xlow)-chart.xScale(0); })
-			    .attr("height",function(d){ return chart.yScale(0)-chart.yScale(d.yup-d.ylow); })
-			    .style("fill", function(d) {return chart.zScale(d.val); });
+      // add the tiles to layers
+      // Adding them before the x-axis
+      chart.layers.tiles = innerG.insert('g', ':first-child')
+        .classed('tiles', true);
 
-		    },
-		    update: function() {
-			// TODO assumes no y-scale change
-			return this;
-		    }
-		}
-	    });
-	},
-	transform: function(data) {
-	    var chart = this;
-	    //cache data 
-	    chart.data = data.data;
-	    var xlowExtent = d3.extent(chart.data, function(d) { return d.xlow; }),
-	    xupExtent = d3.extent(chart.data, function(d) { return d.xup; }),
-	    ylowExtent = d3.extent(chart.data, function(d) { return d.ylow; }),
-	    yupExtent = d3.extent(chart.data, function(d) { return d.yup; });
-	    chart.xScale.domain([xlowExtent[0], xupExtent[1]]);
-	    chart.yScale.domain([ylowExtent[0],yupExtent[1]]);
-	    var zMax = d3.max(chart.data, function(d) { return d.val; });
-	    chart.zScale.domain([0,zMax/2,zMax]);
-	    // (Re)draw the axes as we've changed the scale
-	    chart.drawAxes();
-	    chart.drawColorLabel();
-	    return data;
-	},
-	drawColorLabel: function(transition){
-	    
-	    var chart = this;	    
+      chart.on('change:width', function() {
+        chart.areas.legend
+          .attr('transform', function(d, i) { return 'translate(' + (chart.width() + chart.margins.left + 10) + ',' + chart.margins.top + ')'; });
+      });
 
-	    var nCells = 20;
+      chart.layer('rect', chart.layers.tiles, {
+        dataBind: function(data) {
+          return this.selectAll('rect').data(data.data);
+        },
+        insert: function() {
+          return this.append('rect')
+        .classed('tiles', true);
+        },
+        events: {
+          enter: function() {
+            return this
+              .attr('x', function(d) { return chart.xScale(d.xlow); })
+              .attr('y', function(d) { return chart.yScale(d.yup); })
+              .attr('width', function(d) { return chart.xScale(d.xup - d.xlow) - chart.xScale(0); })
+              .attr('height', function(d) { return chart.yScale(0) - chart.yScale(d.yup - d.ylow); })
+              .style('fill', function(d) { return chart.zScale(d.val); });
+          },
+          update: function() {
+            // TODO assumes no y-scale change
+            return this;
+          }
+        }
+      });
+    },
+    transform: function(data) {
+      var chart = this;
+      //cache data
+      chart.data = data.data;
+      var xlowExtent = d3.extent(chart.data, function(d) { return d.xlow; }),
+          xupExtent = d3.extent(chart.data, function(d) { return d.xup; }),
+          ylowExtent = d3.extent(chart.data, function(d) { return d.ylow; }),
+          yupExtent = d3.extent(chart.data, function(d) { return d.yup; });
+      chart.xScale.domain([xlowExtent[0], xupExtent[1]]);
+      chart.yScale.domain([ylowExtent[0], yupExtent[1]]);
+      var zMax = d3.max(chart.data, function(d) { return d.val; });
+      chart.zScale.domain([0, zMax/2, zMax]);
+      // (Re)draw the axes as we've changed the scale
+      chart.drawAxes();
+      chart.drawColorLabel();
+      return data;
+    },
+    drawColorLabel: function() {
+      // TODO configurable cellWidth, tick number?
+      var chart = this,
+          ticks = chart.zScale.ticks(20).reverse(),
+          tickDiff = Math.abs(ticks[0] - ticks[1]),
+          cellWidth = 25,
+          cellHeight = chart.height()/ticks.length,
+          legendItem = chart.areas.legend.selectAll('.legend-item')
+            .data(ticks)
+            .enter().append('g')
+            .attr('class', 'legend-item')
+            .attr('transform', function(d, i) { return 'translate(0, ' + (i*cellHeight) + ')'; });
 
-	    var legendItem = chart.areas.legend.selectAll(".legend")
-	    	.data(chart.zScale.ticks(20).slice(0).reverse())
-		.enter().append("g")
-		.attr("class","legend")
-	    	.attr("transform", function(d,i){return "translate("+(chart.width() + 70)+"," +(chart.height()/nCells + i *chart.height()/nCells)+")";});	    
-	    
-	    legendItem.append("rect")
-		.attr("width",chart.height()/nCells)
-		.attr("height",chart.height()/nCells)
-		.style("fill",chart.zScale);
+      // Draw colour cells
+      legendItem.append('rect')
+        .attr('width', cellWidth)
+        .attr('height', cellHeight)
+        .style('fill', chart.zScale);
 
-	    legendItem.append("text")
-		.attr("x", chart.height()/nCells+5)
-		.attr("y", 10)
-		.attr("dy", ".35em")
-		.text(String);
-	}
-	
-    });
+      // Draw tick label centered within and offset from the cell
+      legendItem.append('text')
+        .attr('x', cellWidth + 5)
+        .attr('y', (cellHeight)/2)
+        .attr('dy', '.35em')
+        .text(String);
+
+      // Draw bounding box around colour scale
+      // We don't normally assume styles, but a fill certainly isn't desirable
+      chart.areas.legend.append('rect')
+        .attr('width', cellWidth)
+        .attr('height', cellHeight*ticks.length)
+        .classed('legend-box', true)
+        .style('fill', 'none');
+    }
+  });
 })(window.d3);
 ;(function(d3, undefined) {
   'use strict';
   d3.chart('Histogram').extend('HistogramZoom', {
     initialize: function() {
       var chart = this;
+      chart.base.classed('HistogramZoom', true);
 
       // Add a clipping path to hide histogram outside chart area
       var clipRect = chart.base.append('defs').append('clipPath')

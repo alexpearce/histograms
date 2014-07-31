4cd438d69c4c900bd8b5ffd11996919d7e18962f
Alex Pearce <alex@alexpearce.me>
2014-07-31 14:34:47 +0100

    Fix bug where lowest bin wasn't drawn.

--- src/d3.plotable.Histogram.js
+++ src/d3.plotable.Histogram.js
@@ -22,6 +22,9 @@
  *
  * * `color`: The color of the histogram line as a CSS-compatiable string
  *            (default: '#261196')
+ * * `yMinimum`: The minimum value shown on the y-axis.
+ *               If the extent of the bin values is lower than yMinimum,
+ *               yMinimum will be overrided (default: 0)
  */
 (function(d3, undefined) {
   'use strict';
@@ -33,6 +36,9 @@
     if (config.color === undefined) {
       config.color = '#261196';
     }
+    if (config.yMinimum === undefined) {
+      config.yMinimum = 0;
+    }
     return {
       name: name,
       data: data,
@@ -42,13 +48,14 @@
         // Add a 5% padding around the extent for aesthetics.
         var xExtent = d3.extent(this.data, function(d) { return d.xlow; }),
             dxExtent = d3.extent(this.data, function(d) { return d.xhigh; }),
-            xPadding = 0.05*Math.abs(xExtent[0] - xExtent[1]);
+            xPadding = 0.05*Math.abs(xExtent[0] - dxExtent[1]);
         return [xExtent[0] - xPadding, dxExtent[1] + xPadding];
       },
       yDomain: function() {
         // Add a 5% padding at the top of the y extent
-        var yExtent = d3.extent(this.data, function(d) { return d.y; });
-        return [yExtent[0], 1.05*yExtent[1]];
+        var yExtent = d3.extent(this.data, function(d) { return d.y; }),
+            minimum = Math.min(config.yMinimum, yExtent[0]);
+        return [minimum, 1.05*yExtent[1]];
       },
       draw: function(axes, g, transition) {
         if (arguments.length === 0) {
@@ -59,12 +66,18 @@
           transition = false;
         }
         g.classed('Histogram', true);
+        // Prepend a datum to the data for drawing, defining the beginning of
+        // the line area
+        // If this datum is not added, the lower edge of the first bin is hidden
+        var firstBinEdge = data[0].xlow,
+            zeroEl = [{xlow: firstBinEdge, xhigh: firstBinEdge, y: this.yDomain()[0], yerr: 0}],
+            drawnData = zeroEl.concat(this.data);
         var linearea = d3.svg.area()
               .interpolate('step-before')
               .x(function(d) { return axes.xScale(d.xhigh); })
               .y1(function(d) { return axes.yScale(d.y); })
               .y0(function(d) { return d3.max(axes.yScale.range()); }),
-            join = g.selectAll('path').data([this.data]),
+            join = g.selectAll('path').data([drawnData]),
             selection = transition === true ? join.transition().duration(250) : join;
         join.enter()
           .append('path')
